d087870ce2a06ce49e60cbffbb37c4412844273f
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-05-16 18:09:16 +0530

    Hero Section Large screen

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,35 +2,43 @@ import React from "react";
 import "./Hero.css";
 function Hero() {
   return (
-    <div className="block__wrapper">
-      <div className="block-left__wrapper">
-        <div className="block-left">
+    <div className="container">
+      <div className="left">
+        <div className="block left">
           <h2>
-            Mint <span>BLOCKx</span>
+            MINT <span>BLOCKx</span>{" "}
           </h2>
           <h3>
-            9900 / <span>1000</span>
+            9990 / <span>10000</span>
           </h3>
-          <div className="cal__sec">
-            <button>+</button>
-            <p>1</p>
-            <button>-</button>
+          <div className="cal__wrapper">
+            <h2>
+              <span>+</span>
+            </h2>
+            <h2>1</h2>
+            <h2>
+              <span>-</span>
+            </h2>
           </div>
         </div>
-        <div className="button__wrapper">
-          <button className="block__button">Mint</button>
+        <div className="button-left">
+          <button>Mint</button>
         </div>
       </div>
-
-      <div className="block-right">
-        <h2>
-          Earn<span>ings</span>
-        </h2>
-        <h1>
-          9900 <span>Bx</span>
-        </h1>
-        <div className="button__wrapper">
-          <button className="block__button">Claim</button>
+      <div className="right">
+        <div className="block right">
+          <h2>
+            Earn<span>ings</span>
+          </h2>
+          <h1>
+            100 <span>BX</span>
+          </h1>
+          <p>
+            To <span>earn BX </span>hold your NFTs
+          </p>
+        </div>
+        <div className="button-right ">
+          <button>claim</button>
         </div>
       </div>
     </div>
